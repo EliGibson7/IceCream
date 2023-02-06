94e2e7c9e2145b3ffc5f38ed44790df45aef87d4
egibson <egibson5977@W10-5SB6NL3>
2023-02-06 13:19:48 -0600

    Add project files.

---
+++ IceCream/Main Form.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listFlavors = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'listFlavors
+        '
+        Me.listFlavors.FormattingEnabled = True
+        Me.listFlavors.Items.AddRange(New Object() {"Cookies and Cream", "Strawberry", "Chocolate", "Vanilla", "Raspberry"})
+        Me.listFlavors.Location = New System.Drawing.Point(495, 201)
+        Me.listFlavors.Name = "listFlavors"
+        Me.listFlavors.Size = New System.Drawing.Size(120, 82)
+        Me.listFlavors.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.IceCream.My.Resources.Resources.IceCream
+        Me.PictureBox1.Location = New System.Drawing.Point(97, 132)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(304, 222)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.listFlavors)
+        Me.Name = "frmMain"
+        Me.Text = "Ice Cream"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents listFlavors As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ IceCream/Main Form.vb
@@ -0,0 +1,34 @@
+﻿Public Class frmMain
+    Private Sub listFlavors_Click(sender As Object, e As EventArgs) Handles listFlavors.Click
+
+
+
+    End Sub
+
+    Private Sub listFlavors_SelectedIndexChanged(sender As Object, e As EventArgs) Handles listFlavors.SelectedIndexChanged
+
+        If listFlavors.SelectedItem Is "Vanilla" Then
+            PictureBox1.Image = My.Resources.VanillaIceCream
+        End If
+
+        If listFlavors.SelectedItem Is "Chocolate" Then
+            PictureBox1.Image = My.Resources.ChocolateIceCream
+        End If
+
+        If listFlavors.SelectedItems Is "Strawberry" Then
+            PictureBox1.Image = My.Resources.StrawberryIceCream
+        End If
+
+        If listFlavors.SelectedItem Is "Cookies and Cream" Then
+            PictureBox1.Image = My.Resources.CookiesandCreamIceCream
+        End If
+
+        If listFlavors.SelectedItem Is "Raspberry" Then
+            PictureBox1.Image = My.Resources.RaspberryIceCream
+        End If
+
+
+
+
+    End Sub
+End Class
